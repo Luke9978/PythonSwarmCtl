30c1d1604de15b8e16d70e7c509519b092e241f7
Nick Schaup <nschaup@rokcets.utorledo.edu>
2019-03-24 22:40:39 -0400

    Revised Motor Hat Code

--- dc_generic.py
+++ dc_generic.py
@@ -1,24 +1,20 @@
-import time
-import pigpio
+import RPi.GPIO as GPIO
 import curses
-import os
-from prompt_toolkit import prompt
-#set up of PIGPIO daemeaon
-os.system("sudo pigpiod")
-time.sleep(1)
-pi = pigpio.pi()
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 #Speed Varibles Pins
-PWM1 = 12
-PWM2 = 13
-#Speed Varibles with PWM
-Max_Val = 2500  #Also True Values for Directional Varibles
-Min_Val = 0     #Also False Value for Directional Varibles
+PWM1 = GPIO.PWM(12, 50)
+PWM1.start(0)# This is the start value for the duty cycle
+PWM2 = GPIO.PWM(13, 50)
+PWM2.start(0)# This is the start value for the duty cycle
 #Directional Varibles
 D1 = 24
 D2 = 26
 #Setup for Curses
-screen =  curses.initscr()
+screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(1)
@@ -28,24 +24,33 @@ while True:
         if char == ord('q'):
             break
         elif char == ord('w'):
+            GPIO.output(D1, True)
+            GPIO.output(D2, True)
             print('Direction Set Forward')
         elif char == ord('x'):
             print('Direction Set Backward')
+            GPIO.output(D1, False)
+            GPIO.output(D2, False)
         elif  char == ord('q'):
             print('Speed Set High left')
+            PWM1.ChangeDutyCycle(50)
         elif char == ord('e'):
             print('Speed set High Right')
+            PWM2.ChangeDutyCycle(50)
         elif char == ord('a'):
             print('Speed set Medium Left')
-        elif char == ord ('d'):
+            PWM1.ChangeDutyCycle(30)
+        elif char == ord('d'):
             print('Speed Set Medium Right')
+            PWM2.ChangeDutyCycle(30)
         elif char == ord('z'):
             print('Speed set Low Left')
+            PWM1.ChangeDutyCycle(10)
         elif char == ord('c'):
             print('Speed set Low Right')
-
+            PWM2.ChangeDutyCycle(10)
 #Cleanup
-pi.stop()
+GPIO.cleanup
 curses.nobreak()
 screen.keypad(0)
 curses.echo()
